1cea009ea753d8b5c7164ba4a951837e4d09c521
Prajjwal <prajjwal199275@gmail.com>
2022-03-09 23:06:46 +0530

    Implemented rollDice() method

--- src/ThrowDice.java
+++ src/ThrowDice.java
@@ -6,7 +6,12 @@ Time: 17:40
 File: RandomDice.java */
 
 public class ThrowDice {
-    public static void main(String[] args) {
+    public DiceOutcomes rollDice() {
+        var randomOutcome = Double.valueOf(Math.random() * DiceOutcomes.values().length).intValue();
 
+        for (DiceOutcomes diceOutcomes : DiceOutcomes.values()) {
+            if (randomOutcome == diceOutcomes.ordinal())
+                return randomOutcome;
+        }
     }
 }
